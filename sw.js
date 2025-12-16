4c63b17f0371d0899b60acf87b144828fc7c8ba8
sumot519-jpg <sumot519@gmail.com>
2025-12-16 18:34:03 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,53 @@
+// Service Worker for Galactic Fitness PWA
+const CACHE_NAME = 'galactic-fitness-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/dashboard.html',
+    '/styles.css',
+    '/script.js',
+    '/assets/logo.svg',
+    '/assets/favicon.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch assets from cache
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
+
+// Update Service Worker
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
